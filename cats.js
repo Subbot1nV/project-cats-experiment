8d3d7593201f18e9318d20add9f2aef3ca68b8df
Subbot1nv <opt1k@inbox.ru>
2023-02-28 19:27:32 +0300

    добавил котов

---
+++ cats.js
@@ -0,0 +1,83 @@
+const cats = [
+    {
+        "id": 1,
+        "name": "Тима",
+        "age": 10,
+        "description": "Красивый котик Тима. Просит постоянно кушать",
+        "favorite": false,
+        "img_link": "https://i2.wp.com/cdn.lifehacker.ru/wp-content/uploads/2015/10/Depositphotos_5211980_m-2015_1444772010.jpg",
+        "rate": 10
+    },
+    {
+        "id": 2,
+        "name": "Мусёк",
+        "age": 13,
+        "description": "Очень добрый и ласковый",
+        "favorite": false,
+        "img_link": "https://gamerwall.pro/uploads/posts/2022-09/1662427191_3-gamerwall-pro-p-serii-kot-s-zelenimi-glazami-krasivo-4.jpg",
+        "rate": 10
+    },
+    {
+        "id": 3,
+        "name": "Фрося",
+        "age": 9,
+        "description": "Любит греться на солнышке",
+        "favorite": false,
+        "img_link": "https://do-slez.com/uploads/posts/2019-03/1552685275_s1200.jpg",
+        "rate": 10
+    },
+    {
+        "id": 4,
+        "name": "Стася",
+        "age": 1,
+        "description": "Очень красивая и милая кошечка. Любит покушать и молоко. Часто мяукает",
+        "img_link": "https://koshka.top/uploads/posts/2021-11/thumbs/1637794424_5-koshka-top-p-shotlandskaya-visloukhaya-koshka-belaya-5.jpg",
+        "rate": 10,
+        "favorite": false
+    },
+    {
+        "id": 5,
+        "name": "Пёсель",
+        "age": 5,
+        "description": "Просит бросить игрушку и приносит их обратно",
+        "favorite": false,
+        "img_link": "https://sun9-20.userapi.com/impf/c846521/v846521744/202055/J7sC4-lE_fY.jpg?size=1024x731&quality=96&sign=f887782aecbe6511948dc19a534d40e2&c_uniq_tag=9_7AKnDyIjeJoTUAjCpdFfwD76qigNlHQxKNaPrYojM&type=album",
+        "rate": 7
+    },
+    {
+        "id": 6,
+        "name": "Вилли",
+        "favorite": false,
+        "rate": 10,
+        "age": 1,
+        "description": "Играется 24 часа в сутки",
+        "img_link": "https://i.pinimg.com/originals/5b/9a/e7/5b9ae78e9c2f2728036d4c7c17f12ecb.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Тёма",
+        "favorite": false,
+        "rate": 10,
+        "age": 7,
+        "description": "Часто смотрит телевизор",
+        "img_link": "https://chudo-prirody.com/uploads/posts/2021-08/1628827947_177-p-foto-shotlandskikh-visloukhikh-kotov-197.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Кукусик",
+        "favorite": false,
+        "rate": 7,
+        "age": 5,
+        "description": "Гроза всех ёлок",
+        "img_link": "https://koshka.top/uploads/posts/2021-12/1638676900_4-koshka-top-p-kotik-i-yelka-4.jpg"
+    },
+    {
+        "name": "Барсик",
+        "img_link": "https://mixnews.lv/wp-content/uploads/2021/02/27/2021-02-27-mixnews-pisco-the-cat1.jpg",
+        "age": 4,
+        "rate": 8,
+        "favourite": true,
+        "description": "Котик из Шрека",
+        "id": 9
+    }
+]
